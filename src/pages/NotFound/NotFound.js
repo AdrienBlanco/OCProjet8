1d77e687a9604c8ea40a10a4f5345fbb47bb3a71
AdrienBlanco <adrienblanco89@gmail.com>
2023-08-11 09:28:21 +0200

    structure

---
+++ src/pages/NotFound/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div className='test'>
+            <h1>TEst</h1>
+            <p>mais oui</p>
+        </div>
+    )
+}
