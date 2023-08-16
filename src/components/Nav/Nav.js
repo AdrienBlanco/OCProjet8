301278868673bcaaa4ad1ae1e084e47ae12d00f3
AdrienBlanco <adrienblanco89@gmail.com>
2023-08-16 23:31:03 +0200

    indentation

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,6 +3,7 @@ import { Link, useLocation } from "react-router-dom";
 export default function Nav() {
     const location = useLocation();
 
+    // Link et useLocation permettent de renvoyer vers les pages correspondantes et d'afficher la classe "nav--active" en fonction de l'emplacement actuel
     return (
         <nav>
             <Link className={location.pathname === "/" ? "nav--active" : ""} to="/">Accueil</Link>
